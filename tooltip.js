2b8cf94fcc9236b91531ed17912d08d9038eee7c
yuchehseih <hsiehdanny860605@gmail.com>
2019-01-20 18:10:42 +0800

    [IMPORTANT] 利用 getAttribute hasAttribute 去判斷及改變要顯示的內容

--- tooltip.js
+++ tooltip.js
@@ -3,9 +3,13 @@ class Tooltip extends HTMLElement {
     super();
 
     this._tooltipContainer;
+    this._tooltipText = 'some dummy tooltip text!';
   }
 
   connectedCallback() {
+    if (this.hasAttribute('text')) {
+      this._tooltipText = this.getAttribute('text');
+    }
     const tooltipIcon = document.createElement('span');
     tooltipIcon.textContent = ' (?)';
     tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
@@ -15,7 +19,7 @@ class Tooltip extends HTMLElement {
 
   _showTooltip() {
     this._tooltipContainer = document.createElement('div');
-    this._tooltipContainer.textContent = 'This is the tooltip text!';
+    this._tooltipContainer.textContent = this._tooltipText;
     this.appendChild(this._tooltipContainer);
   }
 
