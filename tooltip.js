627a7f8a88b9e86f60db9d6bca06ad12164528d2
yuchehseih <hsiehdanny860605@gmail.com>
2019-01-21 14:53:29 +0800

    [IMPORTANT] 利用 HTML Template 為 custom element 加上 template，但是 tag 內的文字還是沒有出現

--- tooltip.js
+++ tooltip.js
@@ -5,14 +5,15 @@ class Tooltip extends HTMLElement {
     this._tooltipContainer;
     this._tooltipText = 'some dummy tooltip text!';
     this.attachShadow({ mode: 'open' });
+    const template = document.querySelector('#tooltip-template');
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 
   connectedCallback() {
     if (this.hasAttribute('text')) {
       this._tooltipText = this.getAttribute('text');
     }
-    const tooltipIcon = document.createElement('span');
-    tooltipIcon.textContent = ' (?)';
+    const tooltipIcon = this.shadowRoot.querySelector('span');
     tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
     this.shadowRoot.appendChild(tooltipIcon);
