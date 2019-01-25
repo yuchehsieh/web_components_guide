6cb101a8b503fa571917769b4ea3d0729c0191a0
yuchehseih <hsiehdanny860605@gmail.com>
2019-01-25 18:02:32 +0800

    [IMPORTANT] attribute 改變時，更新 this._toolTip

--- tooltip.js
+++ tooltip.js
@@ -64,7 +64,12 @@ class Tooltip extends HTMLElement {
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    console.log(name, oldValue, newValue);
+    if (oldValue === newValue) {
+      return;
+    }
+    if (name === 'text') {
+      this._tooltipText = newValue;
+    }
   }
 
   static get observedAttributes() {
