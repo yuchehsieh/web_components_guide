84319e87a66bd6ba7487cc11b6757bd1bc526876
yuchehseih <hsiehdanny860605@gmail.com>
2019-01-28 12:20:38 +0800

    [重要] 透過 CSS attrubute selector 去改變 modal 得 style。其中如果必須做 style 改變之外的事，就得用 attributeChangedCallback

--- modal.js
+++ modal.js
@@ -16,6 +16,12 @@ class Modal extends HTMLElement {
                 opacity: 0;
                 pointer-events: none;
             }
+
+            :host([opened]) #backdrop, 
+            :host([opened]) #modal {
+                opacity: 1;
+                pointer-events: all;
+            }
             
             #modal {
                 position: fixed;
@@ -73,6 +79,21 @@ class Modal extends HTMLElement {
         </div>
     `;
   }
+
+  //   attributeChangedCallback(name, oldValue, newValue) {
+  //     if (name === 'opened') {
+  //       if (this.hasAttribute('opened')) {
+  //         this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
+  //         this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
+  //         this.shadowRoot.querySelector('#modal').style.opacity = 1;
+  //         this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
+  //       }
+  //     }
+  //   }
+
+  //   static get observedAttributes() {
+  //     return ['opened'];
+  //   }
 }
 
 customElements.define('mh-modal', Modal);
